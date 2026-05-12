09aad4ec325cc478a0864024800e2f7d01db08d7
Welton Carvalho <welton.carvalho.dev@gmail.com>
2026-05-12 13:23:05 -0300

    fix: remove / at the address and so google can index it correctly

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -8,7 +8,7 @@ export function GET() {
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>${PUBLIC_SITE_URL}/</loc>
+    <loc>${PUBLIC_SITE_URL}</loc>
     <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>1.0</priority>
